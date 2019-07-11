665b7bd348ecff0594fb5c5d116e76ea5e415a89
Kevin Trebing <Kevin.Trebing@gmx.net>
2019-07-11 16:34:57 +0100

    Created recursive call to create most likely sentence continuation

--- probabilities.py
+++ probabilities.py
@@ -3,35 +3,47 @@ from torch.nn.functional import softmax
 from gpt2 import GPT2LanguageModel
 
 
+def get_next_words(model, context, words, depth):
+    # get next "word" given context
+    if depth == 0:
+        return words
+    new_words = []
+    for i, word in enumerate(words):
+        logits = model.predict(context, word)
+        # take the one with the highest probability
+        next_word_logit, next_index = logits.topk(1)
+        next_word = model[next_index.item()]
+
+        new_words.append(word + next_word)
+    return get_next_words(model, context, new_words, depth-1)
+
+
 def main():
     model_117M = GPT2LanguageModel(model_name='117M')
     model_345M = GPT2LanguageModel(model_name='345M')
 
-    previous_str = "Global warming is a"
+    model_name = "345M"
+    model = model_117M if model_name == "117M" else model_345M
+
+    context = "Global warming is a"
     next_str = None
 
     topk = 10
 
-    logits = None
-    model_name = "345M"
-    if model_name == "117M":
-        logits = model_117M.predict(previous_str, next_str)
-    elif model_name == "345M":
-        logits = model_345M.predict(previous_str, next_str)
+    logits = model.predict(context, next_str)
 
     probabilities = softmax(logits, dim=-1)
 
     best_logits, best_indices = logits.topk(topk)
-    best_words = []
-    if model_name == "117M":
-        best_words = [model_117M[idx.item()] for idx in best_indices]
-    elif model_name == "345M":
-        best_words = [model_345M[idx.item()] for idx in best_indices]
+    best_words = [model[idx.item()] for idx in best_indices]
+
+    most_likely_words = get_next_words(model, context, best_words, 1)
+
     best_probabilities = probabilities[best_indices].tolist()
 
-    print("Input: ", previous_str)
+    print("Input: ", context)
     for i, prob in enumerate(best_probabilities):
-        print(f"{best_words[i]:12} {prob*100:.3f}%")
+        print(f"{prob*100:.3f}%: {most_likely_words[i].strip()}")
 
 
 if __name__ == '__main__':
